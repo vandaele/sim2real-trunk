48643928095456655f1e17cb10c014960321e7e4
Andrea Protopapa <protopapa.andrea@libero.it>
2024-02-22 19:17:41 +0100

    new setup and functionalities

--- arduino/trunkCalibration/trunkCalibration.ino
+++ arduino/trunkCalibration/trunkCalibration.ino
@@ -8,6 +8,7 @@
 SoftwareSerial   servo_serial(PIN_SW_RX, PIN_SW_TX);
 HerkulexServoBus herkulex_bus(servo_serial);
 HerkulexServo    *servo [8];
+uint16_t servo_positions[8];
 
 unsigned long last_update = 0;
 unsigned long now = 0;
@@ -22,6 +23,158 @@ const uint16_t SINIT = SMIN+abs(SMAX-SMIN)/4;
 const uint16_t LINIT = LMIN-abs(LMIN-LMAX)/4;
 const uint16_t RANGE = 25;
 
+const uint8_t NB_ACTIONS = 100;
+uint8_t actions[NB_ACTIONS] = {5, 12, 12, 12, 12, 12, 7, 7, 7, 3, 3, 3, 3, 13, 13, 13, 5, 5, 5, 5, 4, 4, 4, 0, 0, 0, 2, 13, 13, 13, 13, 6, 4, 4, 4, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 6, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13, 13};
+
+void translate_actions(){
+  for(uint8_t i = 0; i < NB_ACTIONS; i++) {
+        if (actions[i] < 8){
+          actions[i] = (actions[i] + 4) % 8;
+        }
+        else{
+          
+        }
+      }
+}
+
+void displayCables_byFeedback(){
+  for(uint8_t i = 0; i < NB_CABLES; i++) {
+        Serial.print(i);
+        Serial.print(") ");
+        Serial.print(servo[i]->getPosition());
+        Serial.print(" ");
+        }
+  Serial.println();
+}
+
+void displayCables_byArray(){
+  for(uint8_t i = 0; i < NB_CABLES; i++) {
+        Serial.print(i);
+        Serial.print(") ");
+        Serial.print(servo_positions[i]);
+        Serial.print(" ");
+        }
+  Serial.println();
+}
+
+void set_position(uint16_t i, uint16_t position){
+  position = (i<4) ? constrain(position, SMIN, SMAX) : constrain(position, LMAX, LMIN);
+  servo[i]->setPosition(position, 100);
+  servo_positions[i] = position;
+}
+
+void set_position_low(uint16_t i){
+  uint16_t position = (i<4) ? SMIN : LMIN;
+  servo[i]->setPosition(position, 100);
+  servo_positions[i] = position;
+}
+
+void set_position_reset(uint16_t i){ /*TODO for horizontal position */
+  uint16_t position = (i<4) ? SINIT : LINIT;
+  servo[i]->setPosition(position, 100);
+  servo_positions[i] = position;
+}
+
+void pull_cable(uint16_t i){
+  //uint16_t position = servo[cableId]->getPosition();
+  uint16_t position = servo_positions[i];
+  position = (i < 4) ? position+RANGE : position-RANGE;
+  //servo[cableId]->setPosition(position, 100);
+  set_position(i, position);
+  Serial.print(i);
+  Serial.print(") ");
+  Serial.print("+\n");
+}
+
+void release_cable(uint16_t i){
+  //uint16_t position = servo[cableId]->getPosition();
+  uint16_t position = servo_positions[i];
+  position = (i < 4) ? position-RANGE : position+RANGE;
+  //servo[cableId]->setPosition(position, 100);
+  set_position(i, position);
+  Serial.print(i);
+  Serial.print(") ");
+  Serial.print("-\n");
+}
+
+
+void action_to_command(uint16_t action) {
+    uint16_t cableId;
+    switch(action) {
+        case 0:
+            cableId = 0;
+            pull_cable(cableId);
+            break;
+        case 1:
+            cableId = 1;
+            pull_cable(cableId);
+            break;
+        case 2:
+            cableId = 2;
+            pull_cable(cableId);
+            break;
+        case 3:
+            cableId = 3;
+            pull_cable(cableId);
+            break;
+        case 4:
+            cableId = 4;
+            pull_cable(cableId);
+            break;
+        case 5:
+            cableId = 5;
+            pull_cable(cableId);
+            break;
+        case 6:
+            cableId = 6;
+            pull_cable(cableId);
+            break;
+        case 7:
+            cableId = 7;
+            pull_cable(cableId);
+            break;
+        case 8:
+            cableId = 0;
+            release_cable(cableId);
+            break;
+        case 9:
+            cableId = 1;
+            release_cable(cableId);
+            break;
+        case 10:
+            cableId = 2;
+            release_cable(cableId);
+            break;
+        case 11:
+            cableId = 3;
+            release_cable(cableId);
+            break;
+        case 12:
+            cableId = 4;
+            release_cable(cableId);
+            break;
+        case 13:
+            cableId = 5;
+            release_cable(cableId);
+            break;
+        case 14:
+            cableId = 6;
+            release_cable(cableId);
+            break;
+        case 15:
+            cableId = 7;
+            release_cable(cableId);
+            break;
+    }
+}
+
+void simulate_policy(){
+  for(uint8_t i = 0; i<NB_ACTIONS; i++) {
+    action_to_command(actions[i]);
+    delay(1000);
+  }
+}
+
 void setup() {
   Serial.begin(115200);
   servo_serial.begin(115200);
@@ -31,8 +184,9 @@ void setup() {
     servo[i]->setTorqueOn();  // turn power on
     Serial.print(i);
     Serial.print(") ");
-    uint16_t init = (i<4) ? SINIT : LINIT;
+    uint16_t init = (i<4) ? SMIN : LMIN;
     servo[i]->setPosition(init);
+    servo_positions[i] = init;
     Serial.print(init);
     Serial.print(" ");
   }
@@ -46,35 +200,42 @@ void loop() {
   if (Serial.available() > 0) {
     char c = Serial.read();
 
+    Serial.print("Selected cable: ");
+    Serial.println(cableId);
+    displayCables_byArray();
+
     String ids = "012345678";
     int isid = ids.indexOf(c);
     if (isid > -1) {
         cableId = c-'0';
         Serial.print("Selected cable: ");
         Serial.println(cableId);
+        displayCables_byArray();
     }
     else if(c == 'p') {
-        // pull cable
-        uint16_t position = servo[cableId]->getPosition();
-        position = (cableId < 4) ? position+RANGE : position-RANGE;
-        servo[cableId]->setPosition(position, 100);
-        Serial.print("+");
+        // pull cable (plus)
+        pull_cable(cableId);
     }
     else if(c=='m') {
-        // release cable
-        uint16_t position = servo[cableId]->getPosition();
-        position = (cableId < 4) ? position-RANGE : position+RANGE;
-        servo[cableId]->setPosition(position, 100);
-        Serial.print("-");
+        // release cable (minus)
+        release_cable(cableId);
     }
     else if(c=='a') {
-        for(uint8_t i = 0; i < NB_CABLES; i++) {
-        Serial.print(i);
-        Serial.print(") ");
-        Serial.print(servo[i]->getPosition());
-        Serial.print(" ");
-        }
-        Serial.println();
+        displayCables_byArray();
     }
+    else if(c=='l') {
+        for(uint8_t i = 0; i<NB_CABLES; i++) {
+          set_position_low(i);
+        }
+    }
+    else if(c=='r') {
+        for(uint8_t i = 0; i<NB_CABLES; i++) {
+          set_position_reset(i);
+        }
+    }
+    else if(c=='s') {
+        simulate_policy();
+    }
+  displayCables_byArray();
   }
 }
