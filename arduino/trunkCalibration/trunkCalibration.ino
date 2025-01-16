9b19206f66602866c2526201003adcb9eef461d8
Julien Vandaele <julien.vandaele@inria.fr>
2025-01-16 15:20:02 +0100

    arduino: refactor calibration sketch

--- arduino/trunkCalibration/trunkCalibration.ino
+++ arduino/trunkCalibration/trunkCalibration.ino
@@ -5,28 +5,24 @@
 #define PIN_SW_RX 10
 #define PIN_SW_TX 11
 
-SoftwareSerial   servo_serial(PIN_SW_RX, PIN_SW_TX);
-HerkulexServoBus herkulex_bus(servo_serial);
-HerkulexServo    *servo [8];
-uint16_t servo_positions[8];
-
-unsigned long last_update = 0;
-unsigned long now = 0;
-uint8_t cableId = 0;
-
 const uint8_t NB_CABLES = 8;
 const uint16_t SMIN = 297;
 const uint16_t SMAX = 666;
 const uint16_t LMIN = 974;
 const uint16_t LMAX = 297;
-// const uint16_t INIT[NB_CABLES] = {639, 564, 389, 564, 680, 605, 805, 605};
 const uint16_t INIT[NB_CABLES] = {472, 469, 374, 479, 812, 780, 945, 781}; 
-// const uint16_t SINIT = SMIN+abs(SMAX-SMIN)/4;
-// const uint16_t LINIT = LMIN-abs(LMIN-LMAX)/4;
 const uint16_t RANGE = 39;
-
-const unsigned long DELAY = 50;
 const uint8_t PLAYTIME = 100;
+
+SoftwareSerial   servo_serial(PIN_SW_RX, PIN_SW_TX);
+HerkulexServoBus herkulex_bus(servo_serial);
+HerkulexServo    *servo [NB_CABLES];
+uint16_t servo_positions[NB_CABLES];
+
+unsigned long last_update = 0;
+unsigned long now = 0;
+String cableIds = "";
+uint8_t cableId = 0;
 
 void displayCables_byFeedback(){
   for(uint8_t i = 0; i < NB_CABLES; i++) {
@@ -60,7 +56,7 @@ void set_position_low(uint16_t i){
   servo_positions[i] = position;
 }
 
-void set_position_init(uint16_t i){ /*TODO for horizontal position */
+void set_position_init(uint16_t i){
   uint16_t position = INIT[i];
   servo[i]->setPosition(position, 100);
   servo_positions[i] = position;
@@ -78,40 +74,31 @@ void release_cable(uint16_t i){
   set_position(i, position);
 }
 
-void setup_motors() {
+// the setup function runs once when you press reset or power the board
+void setup(){
+  for(uint8_t i = 0; i < NB_CABLES; i++){
+      cableIds.concat(i);
+  }
   Serial.begin(115200);
   servo_serial.begin(115200);
   delay(500);
-  Serial.println("Setup done");
   for(uint8_t i = 0; i<NB_CABLES; i++) {
     servo[i] = new HerkulexServo(herkulex_bus, i);
     servo[i]->setTorqueOn();  // turn power on
-    // Serial.print(i);
-    // Serial.print(") ");
-    // uint16_t init = (i<4) ? SMIN : LMIN;
-    // servo[i]->setPosition(init, PLAYTIME); // set position to low position
-    // servo_positions[i] = init;
-    // Serial.print(init);
-    // Serial.print(" ");
   }
-  Serial.println();
   delay(1000);
-  // translate_actions(); // adapt the fixed actions array to servo motors control
+  Serial.println("Setup done");
 }
 
-void loop_motors_gui() {
+// the loop function runs over and over again forever
+void loop(){
   herkulex_bus.update();
 
   if (Serial.available() > 0) {
     char c = Serial.read();
     Serial.print(c);
 
-    // Serial.print("Selected cable: ");
-    // Serial.println(cableId);
-    // displayCables_byArray();
-
-    String ids = "012345678";
-    int isid = ids.indexOf(c);
+    int isid = cableIds.indexOf(c);
     if (isid > -1) {
         // SELECT CABLE
         cableId = c-'0';
@@ -119,12 +106,10 @@ void loop_motors_gui() {
         Serial.println(cableId);
     }
     else if(c == 'p') {
-        // pull cable (plus)
         pull_cable(cableId);
         Serial.print("+ ");
     }
     else if(c=='m') {
-        // release cable (minus)
         release_cable(cableId);
         Serial.print("- ");
     }
@@ -138,6 +123,7 @@ void loop_motors_gui() {
         for(uint8_t i = 0; i<NB_CABLES; i++) {
           set_position_low(i);
         }
+        Serial.println("Low position done");
     }
     else if(c=='i') {
         for(uint8_t i = 0; i<NB_CABLES; i++) {
@@ -146,17 +132,7 @@ void loop_motors_gui() {
         Serial.println("Initial position done");
     }
    else{
-      Serial.print("ERROR - Command not recognised");
+      Serial.println("ERROR - Command not recognised");
    }
   }
-}
-
-// the setup function runs once when you press reset or power the board
-void setup(){
-  setup_motors();
-}
-
-// the loop function runs over and over again forever
-void loop(){
-  loop_motors_gui();
 }
